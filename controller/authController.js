5f5719feff570c7a15d9352c0665f6649547fb51
Ahsanmureed <ahsanmureed00@gmail.com>
2024-06-03 16:17:22 +0500

    commit

--- controller/authController.js
+++ controller/authController.js
@@ -91,7 +91,7 @@ const loginUser = async (req,res) => {
 const logoutUser  =async (req,res)=>{
   try{
    
-    await res.clearCookie("token",{ expires: new Date(0) },{
+    await res.clearCookie("token",{
       path:"/",
       sameSite: 'none',
       httpOnly:true,
