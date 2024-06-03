2089bf286cf7a2c9a531a720469621f9acdc180e
Ahsanmureed <ahsanmureed00@gmail.com>
2024-06-03 11:34:19 +0500

    commit

--- database/database.js
+++ database/database.js
@@ -1,7 +1,10 @@
 import mongoose, { connect } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config()
+const URL = process.env.DB_URL
 const Connection =()=>{
       try {
-        const connect = mongoose.connect(process.env.DB_URL);
+        const connect = mongoose.connect(URL);
         console.log("database connected  ");
       } catch (error) {
          console.log(error);
